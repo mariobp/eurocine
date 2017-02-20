9ba79f22691c2aa758a76d4631b26f9d12638d66
Mario Barrios <mariobarrpach@gmail.com>
2017-02-19 23:42:36 -0500

    menu listo

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,15 @@
  $(document).ready(function(){
-      $('.slider').slider({
-        height: 231,
-      });
+    $('.slider').slider({
+      height: 231,
     });
+    $('.social').waypoint(function() {
+        $('.cont-menu').addClass('all-fixed');
+        $('.espacio').css('display', 'none');
+    }, { offset: '40%' });
+
+    $('.social').waypoint(function() {
+      console.log("entro");
+        $('.cont-menu').removeClass('all-fixed');
+        $('.espacio').css('display', 'inherit');
+    }, { offset: '60%' });
+  });
